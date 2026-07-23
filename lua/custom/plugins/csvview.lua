bd1a88b3bc4179a2e4b6cd56d5b71d6f20c9745a
paulapreuss <paula.preuss@rl-institut.de>
2026-07-23 09:19:52 +0200

    Add custom plugins

---
+++ lua/custom/plugins/csvview.lua
@@ -0,0 +1,4 @@
+-- csvview
+-- https://github.com/hat0uma/csvview.nvim
+
+vim.pack.add { 'https://github.com/hat0uma/csvview.nvim' }
---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,7 @@
+-- lazygit
+-- https://github.com/kdheepak/lazygit.nvim
+
+vim.pack.add { 'https://github.com/kdheepak/lazygit.nvim' }
+
+
+vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { desc = "LazyGit" })
